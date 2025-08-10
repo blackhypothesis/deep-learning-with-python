36df5ad494296721fbe85d7ec5c39819bde6b058
Marcel <marcel@perseus.luma>
2025-08-10 19:41:02 +0200

    chapter 8

--- book/chapter_08/kaggle_login.py
+++ book/chapter_08/kaggle_login.py
@@ -1,7 +1,3 @@
-import os
-# os.environ["KAGGLE_USERNAME"] = "marcel.luternauer@swisscom.com"
-# os.environ["KAGGLE_KEY"] = "e57b80a7e1fe1751f7562edf8e3fd4fd"
-
 import kagglehub
 kagglehub.login()
 
